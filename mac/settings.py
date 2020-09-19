aa108bf912a457addaff03abe20e00bd50bee73b
Administrator <jaishivnani30@gmail.com>
2020-09-19 10:45:11 +0530

    migrated to postgre sql database

--- mac/settings.py
+++ mac/settings.py
@@ -77,12 +77,19 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT' : '5432',
     }
 }
+
+
 
 
 # Password validation
--- shop/views.py
+++ shop/views.py
@@ -6,11 +6,18 @@ from math import ceil
 from django.http import HttpResponse
 
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil((n/4) - (n//4))
-    allProds = [[products, range(1, nSlides),nSlides], [products, range(1, nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category', 'id')  #Fetching the Category and id
+    cats = {item['category'] for item in catprods}
+
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1,nSlides), nSlides])
+
     params = {'allProds':allProds}
+
     return render(request, 'shop/index.html', params)
 
 def about(request):
