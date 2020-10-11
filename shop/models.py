ea3a1aa8f767dcde6e72b302c5368cfd19fc3be2
Administrator <jaishivnani30@gmail.com>
2020-10-11 21:25:40 +0530

    Building Checkout, Cart and Tracker page and Alerts in contact page

--- shop/models.py
+++ shop/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50,default="")
-    subcategory = models.CharField(max_length=50,default="")
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images",default="")
+    image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
+
 
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
@@ -26,3 +28,23 @@ class Contact(models.Model):
         return self.name
 
 
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone_number = models.CharField(max_length=111, default='')
+
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
